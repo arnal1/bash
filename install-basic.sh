416153a5b3ebed28014168f8cc3de276ca66c42c
Ahmat Arnal Kastana <41889734+arnal1@users.noreply.github.com>
2022-06-13 16:50:23 +0700

    Add files via upload

---
+++ install-basic.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo apt update
+
+sudo apt install apache2
+
+sudo service apache2 status
+
+sudo apt install mysql-server
+
+sudo apt install php libapache2-mod-php php-mcrypt php-mysql
